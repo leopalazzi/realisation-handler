35f613a70aa143d0666e94e55a96a18c1822b2ac
Leo Palazzi <leo.palazzi@evermaps>
2023-10-24 13:53:42 +0200

    Realisation-handler : Add dockerfile

--- client/next.config.js
+++ client/next.config.js
@@ -2,4 +2,5 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({  output: 'standalone',
+})
